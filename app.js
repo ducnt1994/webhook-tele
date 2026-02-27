2cd735cf42674c3336c94d20836c1c38df798b95
duc.nt4 <duc.nt4@kiotviet.com>
2026-02-27 15:20:06 +0700

    first

---
+++ app.js
@@ -0,0 +1,18 @@
+// app.js
+const express = require('express');
+const router = express.Router({mergeParams: true});
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const port = 3000;
+const webhookService = require('./webhook-service')
+
+app.post('/webhook', async (req, res) => {
+  // call to function of webhook service
+  await webhookService.checkWebhookMessage(req, res);
+  res.send('Webhook message sent');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
---
+++ webhook-service.js
@@ -0,0 +1,13 @@
+const webhookService = {
+  checkWebhookMessage: async (req, res) => {
+    try {
+      console.log("Received webhook message:", req.body);
+      return true
+    } catch (error) {
+      console.error("Error processing webhook message:", error);
+      return false
+    }
+  }
+}
+
+module.exports = webhookService;
